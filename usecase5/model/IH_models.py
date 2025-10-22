7e2fa00ec8b1cf0f5d1fab6160686ef3810b51ce
Nico Dirkes <dirkes@aices.rwth-aachen.de>
2025-10-22 15:49:25 +0200

    add stress-based pore model

--- usecase5/model/IH_models.py
+++ usecase5/model/IH_models.py
@@ -2,6 +2,10 @@ import numpy as np
 from scipy.integrate import quad
 import csv
 
+# define logarithmic function and inverse
+logF = np.log
+invLogF = np.exp
+
 # Polynomial coefficients for the area strain to pore area conversion (fifth order polynomial fit)
 p = [ 4.06157696e-02, -2.83266089e-05,  2.25830588e-08, -8.49450091e-13, 1.32415867e-17, -8.23845340e-23]
 
@@ -15,6 +19,19 @@ def computePoreAreaInterpolated(G):
         return 6.1932
     else:
         return p[0] + p[1]*G + p[2]*G**2 + p[3]*G**3 + p[4]*G**4 + p[5]*G**5
+    
+def IH_poreFormation_stressBased(t_exp, sigma_exp, h, k, log=False, mu=0.0035, V_RBC=147.494):
+    """
+    Model #4: Compute IH with pore formation model based on stress-based morphology.
+    """
+    G_exp = sigma_exp / mu  # shear rate
+
+    Apt = computePoreAreaInterpolated(G_exp) * t_exp
+    if log:
+        Apt = max(Apt, 1e-10)  # ensure Apt is not zero for log calculation
+        return -h - logF(V_RBC) + k * logF(G_exp) + logF(Apt) + logF(100)
+    else:
+        return invLogF(-h) * (G_exp ** k) * Apt / V_RBC * 100
 
 def IH_poreFormation_strainBased(t_exp, sigma_exp, h, k, log=False, mu=0.0035, f1=5.0, V_RBC=147.494, analytical=True):
     """
@@ -34,9 +51,9 @@ def IH_poreFormation_strainBased(t_exp, sigma_exp, h, k, log=False, mu=0.0035, f
 
     if log:
         Apt = max(Apt, 1e-10)  # ensure Apt is not zero for log calculation
-        return -h - np.log(V_RBC) + k * np.log(G_exp) + np.log(Apt) + np.log(100)
+        return -h - logF(V_RBC) + k * logF(G_exp) + logF(Apt) + logF(100)
     else:
-        return np.exp(-h) * (G_exp ** k) * Apt / V_RBC * 100
+        return invLogF(-h) * (G_exp ** k) * Apt / V_RBC * 100
 
 def integral_poreFormation_analytical(t_exp, G_exp, f=5.0):
     """
@@ -101,9 +118,9 @@ def IH_powerLaw_strainBased(t_exp, sigma_exp, A, alpha, beta, f1=5.0, log=False,
         G_int, _ = quad(lambda t: Geff_normalized(t) ** (alpha / beta), 0, t_exp)
 
     if log:
-        return -A + alpha * np.log(sigma_exp) + beta * np.log(G_int) + np.log(100)
+        return -A + alpha * logF(sigma_exp) + beta * logF(G_int) + logF(100)
     else:
-        return np.exp(-A) * sigma_exp**alpha * G_int ** beta * 100
+        return invLogF(-A) * sigma_exp**alpha * G_int ** beta * 100
 
 def IH_powerLaw_stressBased(t_exp, sigma_exp, A, alpha, beta, log=False):
     """
@@ -114,9 +131,9 @@ def IH_powerLaw_stressBased(t_exp, sigma_exp, A, alpha, beta, log=False):
         0.01 <= beta <= 1
     """
     if log:
-        return -A + alpha * np.log(sigma_exp) + beta * np.log(t_exp) + np.log(100)
+        return -A + alpha * logF(sigma_exp) + beta * logF(t_exp) + logF(100)
     else:
-        return np.exp(-A) * (sigma_exp ** alpha) * (t_exp ** beta) * 100  # convert to percentage
+        return invLogF(-A) * (sigma_exp ** alpha) * (t_exp ** beta) * 100  # convert to percentage
 
 
 def get_input_data(fname):
@@ -135,7 +152,7 @@ def get_input_data(fname):
 
     return np.array([t_exp, sigma_exp]).T
 
-def get_IH_model(model_name, mu, f1, f2, V_RBC, log=False):
+def get_IH_model(model_name, mu, f1, f2, V_RBC, log, analytical):
     """
     Returns the appropriate IH model function based on the model name.
     """
@@ -149,19 +166,25 @@ def get_IH_model(model_name, mu, f1, f2, V_RBC, log=False):
         def computeIH_powerLaw_strainBased(x, p):
             assert len(p) == 3, "Parameters should contain exactly three values: [A, alpha, beta]"
             assert len(x) == 2, "Input x should contain exactly rows: [t_exp, sigma]"
-            return IH_powerLaw_strainBased(*x, *p, f1, log)
+            return IH_powerLaw_strainBased(*x, *p, f1, log=log, analytical=analytical)
         return computeIH_powerLaw_strainBased
-    elif model_name == 'IH_poreFormation':
+    elif model_name == 'IH_poreFormation_strainBased':
         def computeIH_poreFormation(x, p):
             assert len(p) == 2, "Parameters should contain exactly two values: [h, k]"
             assert len(x) == 2, "Input x should contain exactly rows: [t_exp, sigma]"
-            return IH_poreFormation(*x, *p, log, mu=mu, f1=f1, f2=f2, V_RBC=V_RBC)
+            return IH_poreFormation_strainBased(*x, *p, log=log, mu=mu, f1=f1, f2=f2, V_RBC=V_RBC, analytical=analytical)
         return computeIH_poreFormation
+    elif model_name == 'IH_poreFormation_stressBased':
+        def computeIH_poreFormation_stressBased(x, p):
+            assert len(p) == 2, "Parameters should contain exactly two values: [h, k]"
+            assert len(x) == 2, "Input x should contain exactly rows: [t_exp, sigma]"
+            return IH_poreFormation_stressBased(*x, *p, log=log, mu=mu, f2=f2, V_RBC=V_RBC)
+        return computeIH_poreFormation_stressBased
     else:
         raise ValueError(f"Unknown model name: {model_name}")
 
 def evaluate_model(parameters, fname_controlVars='data.csv', model_name='IH_powerLaw_strainBased', log=False,
-                   mu=0.0035, f1=5.0, f2=4.2298e-4, V_RBC=147.494):
+                   mu=0.0035, f1=5.0, f2=4.2298e-4, V_RBC=147.494, analytical=True):
     """
     Computes IH using the specified model with given parameters.
     Inputs:
@@ -176,7 +199,7 @@ def evaluate_model(parameters, fname_controlVars='data.csv', model_name='IH_powe
     - A list of computed IH values for each control variable point.
     """
 
-    IH_model = get_IH_model(model_name, mu, f1, f2, V_RBC, log)
+    IH_model = get_IH_model(model_name, mu, f1, f2, V_RBC, log, analytical)
     input_data = get_input_data(fname_controlVars)
 
     return [ IH_model(x, parameters) for x in input_data ]
@@ -193,13 +216,13 @@ def main():
     fname_data = 'data.csv'
     output_data_stressBased = evaluate_model(parameters, fname_controlVars=fname_data, model_name='IH_powerLaw_stressBased')
     output_data_strainBased = evaluate_model(parameters, fname_controlVars=fname_data, model_name='IH_powerLaw_strainBased', f1=5.0)
-    output_data_pore = evaluate_model(parameters_pore, fname_controlVars=fname_data, model_name='IH_poreFormation', mu=0.00424, f1=5.0, f2=4.2298e-4, V_RBC=147.494)
-    output_data_pore_log = evaluate_model([np.log(parameters_pore[0]), parameters_pore[1]], fname_controlVars=fname_data, model_name='IH_poreFormation',log=True, mu=0.00424, f1=5.0, f2=4.2298e-4, V_RBC=147.494)
+    output_data_pore_strain = evaluate_model(parameters_pore, fname_controlVars=fname_data, model_name='IH_poreFormation_strainBased', mu=0.00424, f1=5.0, f2=4.2298e-4, V_RBC=147.494)
+    output_data_pore_stress = evaluate_model(parameters_pore, fname_controlVars=fname_data, model_name='IH_poreFormation_stressBased', mu=0.00424, f1=5.0, f2=4.2298e-4, V_RBC=147.494)
 
     print("Output data (stress-based):", output_data_stressBased)
     print("Output data (strain-based):", output_data_strainBased)
-    print("Output data (pore formation):", output_data_pore)
-    print("Output data (pore formation, log):", np.exp(output_data_pore_log))
+    print("Output data (pore formation, strain-based):", output_data_pore_strain)
+    print("Output data (pore formation, stress-based):", output_data_pore_stress)
 
 
 if __name__ == "__main__":
